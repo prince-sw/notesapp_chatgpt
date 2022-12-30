d5d903abc8cf17c0b311669f6497b6efb9b16871
psw <pswargiary101@gmail.com>
2022-12-30 21:06:47 +0530

    inital commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const app = express();
+
+// port = 5000;
+
+// app.get("/api", (req, res) => {
+//   res.json({ users: ["one", "usertwo"] });
+// });
+
+// app.listen(port, () => {
+//   console.log("Listening to port 5000");
+// });
+
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.retrieveModel("text-davinci-003");
+
+app.post("/", async (req, res) => {
+  // console.log(req.body);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  if (response.data) {
+    res.json(response.data.choices[0].text);
+  }
+  // console.log(response.data);
+  // res.json(req.body);
+});
+
+app.listen(3001, () => {
+  console.log("Server listening on port 3001");
+});
