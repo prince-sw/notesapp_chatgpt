a4e8c7fe1709a862eb36c74778e63b427e6aea7f
psw <pswargiary101@gmail.com>
2023-01-07 19:39:45 +0530

    Added chakra ui, need to pass input to backend

--- server/server.js
+++ server/server.js
@@ -1,18 +1,3 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const app = express();
-
-// port = 5000;
-
-// app.get("/api", (req, res) => {
-//   res.json({ users: ["one", "usertwo"] });
-// });
-
-// app.listen(port, () => {
-//   console.log("Listening to port 5000");
-// });
-
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const cors = require("cors");
@@ -33,20 +18,16 @@ const openai = new OpenAIApi(configuration);
 app.post("/", async (req, res) => {
   const inputText = req.body.message;
   console.log(inputText);
-  // const response = await openai.createCompletion({
-  //   model: "text-davinci-003",
-  //   prompt: `Convert the following movie into emoji: ${inputText}`,
-  //   temperature: 0.8,
-  //   max_tokens: 60,
-  // });
-  const response = await openai.createImage({
-    prompt: "A cute baby sea otter",
-    n: 2,
-    size: "1024x1024",
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Convert the following movie into emoji: ${inputText}`,
+    temperature: 0.8,
+    max_tokens: 60,
   });
+
   if (response.data) {
-    // res.json(response.data.choices[0].text);
-    res.json(response.data[0].url);
+    res.json(response.data.choices[0].text);
+    // res.json(response.data[0].url);
   }
   // console.log(response.data);
   // res.json(req.body);
